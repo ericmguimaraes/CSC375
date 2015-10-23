a176cbf08aa9f9d2350fd358df26fa321302e897
Eric Guimarães <ericmguimaraes@gmail.com>
2015-10-22 23:07:13 -0400

    added lockFreeLibrary to the controller

--- Assignment 2 - Library/library/src/main/java/assignment2/library/Controller.java
+++ Assignment 2 - Library/library/src/main/java/assignment2/library/Controller.java
@@ -4,10 +4,11 @@ public class Controller {
 
 	static SynchronizedLibrary syncronizedGameServer;
 
-	static LockFreeLibrary noJDKGameServer;
+	static LockFreeLibrary lockFreeLibrary;
 
 	public static void main(String args[]) {
-		//Main.main(args);
+		// Main.main(args);
 		syncronizedGameServer = new SynchronizedLibrary();
+		lockFreeLibrary = new LockFreeLibrary();
 	}
 }
