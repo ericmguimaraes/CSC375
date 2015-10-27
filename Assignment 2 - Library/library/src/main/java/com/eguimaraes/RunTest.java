d1cebe314ad04f8823913ae1af70a2a9dd411343
Eric Guimarães <ericmguimaraes@gmail.com>
2015-10-27 00:14:12 -0400

    demos

--- Assignment 2 - Library/library/src/main/java/com/eguimaraes/RunTest.java
+++ Assignment 2 - Library/library/src/main/java/com/eguimaraes/RunTest.java
@@ -5,7 +5,7 @@ import org.openjdk.jmh.Main;
 public class RunTest {
  
     public static void main(String[] args) throws Exception {
-        Main.main(getArguments("MyBenchmark", 5, 5, 30));
+        Main.main(getArguments("MyBenchmark", 10, 5, 100));
     }
  
     private static String[] getArguments(String className, int nRuns, int runForMilliseconds, int nThreads) {
@@ -14,7 +14,7 @@ public class RunTest {
             "-r", runForMilliseconds + "ms",
             "-t", "" + nThreads,
             "-w", "5ms",
-            "-wi", "3"
+            "-wi", "5"
         };
     }
 }
